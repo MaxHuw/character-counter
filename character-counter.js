581e6c5c2ec5d22b58f2d59790e3886a673970f0
Max Graham <m.huw.graham@gmail.com>
2019-01-09 03:39:33 +0000

    Function reads input, checks if it is a letter [a-z], and if it is already in the object.

--- character-counter.js
+++ character-counter.js
@@ -18,13 +18,25 @@ ToDo:
 
 **************************************************/
 
-var input = process.argv[2].toString();
-console.log(input);
+//var input = process.argv[2].join().toLowerCase();
+//console.log(input);
+
+console.log(countLetters("this is a test"));
 
 function countLetters(input){
 
   var letters = {};
 
+  for (var i = 0; i < input.length; i ++){
+    if (input[i].match(/[a-z]/) && !(input[i] in letters)){
+      letters[input[i]] = 1;
+    } else if (input[i].match(/[a-z]/) && (input[i] in letters)){
+      letters[input[i]] += 1;
+    } else {continue;}
+  }
 
+  console.log(letters);
+
+  return letters;
 
 };
